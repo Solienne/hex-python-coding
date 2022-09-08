01355c0509afc85c9b4590bf863120e94f8d367f
Solienne <soliene.teixeira@ufba.br>
2022-09-08 21:23:57 +0000

    inheritance

---
+++ 0x06-python-inheritance/3-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defines class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
---
+++ 0x06-python-inheritance/4-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Defines area"""
+    def area(self):
+        """raises Exception"""
+        raise Exception("area() is not implemented")
+    pass
