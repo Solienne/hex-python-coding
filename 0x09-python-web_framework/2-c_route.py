65f7500cc3cdc9f59de2047eeb0bb3b506c1ad59
Solienne <soliene.teixeira@ufba.br>
2022-09-12 12:24:51 +0000

    Web framework

--- 0x09-python-web_framework/2-c_route.py
+++ 0x09-python-web_framework/2-c_route.py
@@ -3,14 +3,19 @@
 """Script starts Flask web Application"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+@app.route("/", strict_slashes=False)
+def hello():
+    return ("Hello HBNB!")
 
 
-@app.route('/')
-def hello_HBNB():
-    return 'Hello HBNB!'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return ("HBNB")
 
 
-@app.route('/hbnb')
-def HBNB():
-    return 'HBNB'
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return ("C " + text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
